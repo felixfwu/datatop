e36b08abbb3553f55baf65e85516a05856207add
Felix Wu <6695028+felixfwu@users.noreply.github.com>
2023-10-16 21:18:18 +0800

    feat: add version command (#10)

--- cmd/cobracli/cmd/root.go
+++ cmd/cobracli/cmd/root.go
@@ -8,6 +8,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:          "datatop",
+	Version:      "",
 	SilenceUsage: true,
 	Short:        "An open source tool for finding top data.",
 	Long:         `An open source tool for finding top data. http://github.com/felixfwu/datatop`,
