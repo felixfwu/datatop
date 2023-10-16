2c4a40b696f261492cf78d7d683ac3aa64895170
Felix Wu <6695028+felixfwu@users.noreply.github.com>
2023-10-16 22:30:30 +0800

    Feat version (#12)
    * feat: add version command
    
    * fix: version does not work
    
    * fix: use embed to inject version number when goreleaser action fire

--- cmd/cobracli/cmd/root.go
+++ cmd/cobracli/cmd/root.go
@@ -3,14 +3,17 @@ package cobracli
 import (
 	"os"
 
+	_ "embed"
+
 	"github.com/spf13/cobra"
 )
 
-var BuildVersion string
+//go:embed VERSION
+var version string
 
 var rootCmd = &cobra.Command{
 	Use:          "datatop",
-	Version:      BuildVersion,
+	Version:      version,
 	SilenceUsage: true,
 	Short:        "An open source tool for finding top data.",
 	Long:         `An open source tool for finding top data. http://github.com/felixfwu/datatop`,
